c7e8b0dc2640e54344045965eb09e0b7d04baf59
Agg <email@em.lc>
2023-09-24 13:12:14 -0300

    fecha derecha

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
-
+function makeArray() {
+    for (i = 0; i<makeArray.arguments.length; i++)
+    this[i + 1] = makeArray.arguments[i];}
+    var months = new makeArray('Enero','Febrero','Marzo','Abril','Mayo',
+    'Junio','Julio','Agosto','Septiembre','Octubre','Noviembre','Diciembre');
     var date = new Date();
-    var d  = date.getDate();
-    var day = (d < 10) ? '0' + d : d;
-    var m = date.getMonth() + 1;
-    var month = (m < 10) ? '0' + m : m;
+    var day = date.getDate();
+    var month = date.getMonth() + 1;
     var yy = date.getYear();
     var year = (yy < 1000) ? yy + 1900 : yy;
-    document.write(day + "/" + month + "/" + year);
+    document.write( day + " de " + months[month] + " del " + year);
